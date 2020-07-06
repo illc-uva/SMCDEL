14469648dc05ee62bbed5ae23082b1d13301c88d
Malvin Gattinger <malvin@w4eg.de>
2020-07-06 19:01:31 +0200

    use CUDD 3.0.0
    Updating CUDD to 3.0.0 via the Haskell binding cudd-0.1.0.4.
    Note: CUDD itself is no longer compiled together with the binding.
    See gitlab-ci.yml for commands to install it globally on Debian.

--- src/SMCDEL/Internal/MyHaskCUDD.hs
+++ src/SMCDEL/Internal/MyHaskCUDD.hs
@@ -13,54 +13,54 @@ module SMCDEL.Internal.MyHaskCUDD (
 
 import qualified Cudd.Cudd
 
-type Bdd = Cudd.Cudd.DdNode
+type Bdd = Cudd.Cudd.DDNode
 
-manager :: Cudd.Cudd.DdManager
+manager :: Cudd.Cudd.DDManager
 manager = Cudd.Cudd.cuddInit
 
 top :: Bdd
-top = Cudd.Cudd.cuddReadOne manager
+top = Cudd.Cudd.readOne manager
 
 bot :: Bdd
-bot = Cudd.Cudd.cuddReadLogicZero manager
+bot = Cudd.Cudd.readLogicZero manager
 
 var :: Int -> Bdd
-var = Cudd.Cudd.cuddBddIthVar manager
+var = Cudd.Cudd.ithVar manager
 
 neg :: Bdd -> Bdd
-neg = Cudd.Cudd.cuddNot manager
+neg = Cudd.Cudd.bNot manager
 
 xor :: Bdd -> Bdd -> Bdd
-xor = Cudd.Cudd.cuddBddXor manager
+xor = Cudd.Cudd.bXor manager
 
 exists :: Int -> Bdd -> Bdd
-exists n b = Cudd.Cudd.cuddBddExistAbstract manager b (Cudd.Cudd.cuddIndicesToCube manager [n])
+exists n b = Cudd.Cudd.bExists manager b (Cudd.Cudd.indicesToCube manager [n])
 
 forall :: Int -> Bdd -> Bdd
-forall n b = Cudd.Cudd.cuddBddUnivAbstract manager b (Cudd.Cudd.cuddIndicesToCube manager [n])
+forall n b = Cudd.Cudd.bForall manager b (Cudd.Cudd.indicesToCube manager [n])
 
 existsSet :: [Int] -> Bdd -> Bdd
 existsSet [] b = b
-existsSet ns b = Cudd.Cudd.cuddBddExistAbstract manager b (Cudd.Cudd.cuddIndicesToCube manager ns)
+existsSet ns b = Cudd.Cudd.bExists manager b (Cudd.Cudd.indicesToCube manager ns)
 
 forallSet :: [Int] -> Bdd -> Bdd
 forallSet [] b = b
-forallSet ns b = Cudd.Cudd.cuddBddUnivAbstract manager b (Cudd.Cudd.cuddIndicesToCube manager ns)
+forallSet ns b = Cudd.Cudd.bForall manager b (Cudd.Cudd.indicesToCube manager ns)
 
 equ :: Bdd -> Bdd -> Bdd
 equ a b = con (imp a b) (imp b a)
 
 imp :: Bdd -> Bdd -> Bdd
-imp b1 b2 = Cudd.Cudd.cuddBddIte manager b1 b2 top
+imp b1 b2 = Cudd.Cudd.bIte manager b1 b2 top
 
 ifthenelse :: Bdd -> Bdd -> Bdd -> Bdd
-ifthenelse = Cudd.Cudd.cuddBddIte manager
+ifthenelse = Cudd.Cudd.bIte manager
 
 con :: Bdd -> Bdd -> Bdd
-con = Cudd.Cudd.cuddBddAnd manager
+con = Cudd.Cudd.bAnd manager
 
 dis :: Bdd -> Bdd -> Bdd
-dis = Cudd.Cudd.cuddBddOr manager
+dis = Cudd.Cudd.bOr manager
 
 conSet :: [Bdd] -> Bdd
 conSet [] = top
@@ -83,9 +83,9 @@ gfp operator = gfpLoop top where
       else gfpLoop (operator current)
 
 restrict :: Bdd -> (Int,Bool) -> Bdd
-restrict b (n,bit) = Cudd.Cudd.cuddBddLICompaction manager b res where
+restrict b (n,bit) = Cudd.Cudd.liCompaction manager b res where
   res = if bit then var n else neg (var n)
 
 restrictSet :: Bdd -> [(Int,Bool)] -> Bdd
-restrictSet b bits = Cudd.Cudd.cuddBddLICompaction manager b res where
+restrictSet b bits = Cudd.Cudd.liCompaction manager b res where
   res = conSet $ map (\(n,bit) -> if bit then var n else neg (var n)) bits
